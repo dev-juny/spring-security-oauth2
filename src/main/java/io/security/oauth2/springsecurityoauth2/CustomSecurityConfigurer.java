6792c4f824c8358fd4e78d36acdb8619baf16a90
juny <king932z@naver.com>
2024-09-19 22:06:21 +0900

    spring-security-oauth2 실습

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/CustomSecurityConfigurer.java
@@ -0,0 +1,19 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+public class CustomSecurityConfigurer extends AbstractHttpConfigurer<CustomSecurityConfigurer, HttpSecurity> {
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        super.init(builder);
+        System.out.println("init method started..");
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        super.configure(builder);
+        System.out.println("configure method started..");
+    }
+}
